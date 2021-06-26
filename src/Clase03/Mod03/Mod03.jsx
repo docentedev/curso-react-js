3ace6d6875c28e3909ad22e7c6c315c1b2d6cca5
User <user@Users-MacBook-Air.local>
2021-06-25 22:59:02 -0400

    Add persist 01

--- src/Clase03/Mod03/Mod03.jsx
+++ src/Clase03/Mod03/Mod03.jsx
@@ -2,6 +2,15 @@ import Tarea from '../../components/Tarea'
 
 const title = 'PERSISTENCIA DE DATOS'
 
+const description = `
+Para persistir primero vamos a crear una escructura de datos que nos permita luego persistirlos todos juntos en el \`localStorage\`
+
+\`npm install redux react-redux\`
+
+[https://github.com/docentedev/c__react-redux-persist](https://github.com/docentedev/c__react-redux-persist)
+
+`.trim()
+
 const Mod03 = () => {
 
     return (
@@ -11,9 +20,9 @@ const Mod03 = () => {
                 <Tarea
                     language="bash"
                     code={``.trim()}
-                    title=""
+                    title="Manejar datos con Redux"
                     number="1"
-                    description={''}
+                    description={description}
                 />
             </div>
         </div>
