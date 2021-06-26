6cc3cb8d329f77d55e7d2e0349a508ab28b418d8
User <user@Users-MacBook-Air.local>
2021-06-25 23:36:03 -0400

    Add redux react example

--- src/Clase03/Mod03/Mod03.jsx
+++ src/Clase03/Mod03/Mod03.jsx
@@ -7,8 +7,11 @@ Para persistir primero vamos a crear una escructura de datos que nos permita lue
 
 \`npm install redux react-redux\`
 
-[https://github.com/docentedev/c__react-redux-persist](https://github.com/docentedev/c__react-redux-persist)
+[ejemplo](https://github.com/docentedev/c__react-redux-persist)
 
+`.trim()
+
+const description01 = `
 `.trim()
 
 const Mod03 = () => {
@@ -24,6 +27,34 @@ const Mod03 = () => {
                     number="1"
                     description={description}
                 />
+                <Tarea
+                    language="bash"
+                    code={``.trim()}
+                    title="Creando un Store con Redux"
+                    number="2"
+                    description={description01}
+                />
+                <iframe
+                    type="text/javascript"
+                    frameborder="0"
+                    style={{ width: "100%", height: "2140px" }}
+                    title="redux_store_counter_module.ts"
+                    src="https://gist.github.com/docentedev/6597a32cc5cc9cd1b5bfafef04184972.pibb"
+                ></iframe>
+                <Tarea
+                    language="bash"
+                    code={``.trim()}
+                    title="Integrando en React con Redux gracias a React Redux"
+                    number="3"
+                    description={''}
+                />
+                <iframe
+                    type="text/javascript"
+                    frameborder="0"
+                    style={{ width: "100%", height: "1300px" }}
+                    title="redux_integrate_with_react.tsx"
+                    src="https://gist.github.com/docentedev/412becebfae5c6a98386ea5b89a328b5.pibb"
+                ></iframe>
             </div>
         </div>
     )
